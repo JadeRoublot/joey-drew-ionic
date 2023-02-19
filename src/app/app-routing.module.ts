d8d3300ece86400b6dbb2fd59322633b0e2f7f47
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-02-19 18:13:30 +0100

    Update
    - creation of user
    - creation of tabs
    - displaying of tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,34 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'movie-list',
-    loadChildren: () => import('./movie-list/movie-list.module').then( m => m.MovieListPageModule)
-  },
-  {
-    path: 'vinyl-list',
-    loadChildren: () => import('./vinyl-list/vinyl-list.module').then( m => m.VinylListPageModule)
-  },
-  {
-    path: 'secret-list',
-    loadChildren: () => import('./secret-list/secret-list.module').then( m => m.SecretListPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'tab',
     loadChildren: () => import('./tab/tab.module').then( m => m.TabPageModule)
   },
+  
 ];
 
 @NgModule({
--- src/app/tab/tab-routing.module.ts
+++ src/app/tab/tab-routing.module.ts
@@ -5,10 +5,40 @@ import { TabPage } from './tab.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: TabPage
-  }
-];
+    path:'tab',
+    component: TabPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'movie',
+        loadChildren: () => import('../movie-list/movie-list.module').then( m => m.MovieListPageModule)
+      },
+      {
+        path: 'vinyl',
+        loadChildren: () => import('../vinyl-list/vinyl-list.module').then( m => m.VinylListPageModule)
+      },
+      {
+        path: 'user',
+        loadChildren: () => import('../user/user.module').then( m => m.UserPageModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('../about/about.module').then( m => m.AboutPageModule)
+      },
+    ]
+   } ,
+   {
+    path:'',
+    redirectTo:'/tab/home',
+    pathMatch:'full'
+   }
+    
+    
+  ];
+  
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
