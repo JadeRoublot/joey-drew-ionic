6a2c4de5b70d76187a905524ef8d8ffb4924546d
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-03-14 16:49:44 +0100

    Update

--- src/app/movie-list/movie/movie.page.ts
+++ src/app/movie-list/movie/movie.page.ts
@@ -9,10 +9,64 @@ import { MovieService } from '../../services/movie.service';
   styleUrls: ['./movie.page.scss'],
 })
 export class MoviePage implements OnInit {
-  modif: boolean = false;
-  constructor() { }
+  modif!: boolean;
+  movie: any = null;
+  constructor(
+    private alertCtrl : AlertController,
+    private route: ActivatedRoute,
+    private Movie: MovieService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.modif = false;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.Movie.get(id).subscribe((value: any) => {
+      this.movie = value;
+    });
+  }
+
+  async setModif() {
+    if(!this.modif) {
+      const alert = await this.alertCtrl.create({
+        header: 'Etes-vous sur de vouloir modifier ?',
+        subHeader: 'Vous rendrez possible la modification',
+        buttons: [
+          {
+            text: 'Annuler',
+            role: 'Cancel'
+          }, {
+            text: 'Confirmer',
+            handler: () => { this.modif = !this.modif}
+          }
+        ]
+      });
+  
+      await alert.present();
+    } else {
+    this.modif = !this.modif;
+    }
+  }
+
+  async presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Vos modifications sont enregistrées',
+      duration: 2000
+    });
+    (await toast).present();
+  }
+
+  onModif() {
+    this.Movie.update(this.movie).subscribe(() => {
+      this.presentToast();
+      this.modif = false;
+    });
+  }
+
+  onDelete(id: any) {
+    this.Movie.delete(id);
+    this.router.navigate(['/tab/movie'])
   }
 
 }
--- src/app/vinyl-list/vinyl/vinyl.page.ts
+++ src/app/vinyl-list/vinyl/vinyl.page.ts
@@ -8,10 +8,64 @@ import { VinylService } from '../../services/vinyl.service';
   styleUrls: ['./vinyl.page.scss'],
 })
 export class VinylPage implements OnInit {
-  modif: boolean = false;
-  constructor() { }
+  modif!: boolean;
+  vinyl: any = null;
+
+  constructor(
+    private alertCtrl : AlertController,
+    private route: ActivatedRoute,
+    private Vinyl: VinylService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.modif = false;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.Vinyl.get(id).subscribe((value: any) => {
+      this.vinyl = value;
+    });
+  }
+  async setModif() {
+    if(!this.modif) {
+      const alert = await this.alertCtrl.create({
+        header: 'Etes-vous sur de vouloir modifier ?',
+        subHeader: 'Vous rendrez possible la modification',
+        buttons: [
+          {
+            text: 'Annuler',
+            role: 'Cancel'
+          }, {
+            text: 'Confirmer',
+            handler: () => { this.modif = !this.modif}
+          }
+        ]
+      });
+  
+      await alert.present();
+    } else {
+    this.modif = !this.modif;
+    }
+  }
+
+  async presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Vos modifications sont enregistrées',
+      duration: 2000
+    });
+    (await toast).present();
+  }
+
+  onModif() {
+    this.Vinyl.update(this.vinyl).subscribe(() => {
+      this.presentToast();
+      this.modif = false;
+    });
+  }
+
+  onDelete(id: any) {
+    this.Vinyl.delete(id);
+    this.router.navigate(['/tab/vinyl'])
   }
 
 }
