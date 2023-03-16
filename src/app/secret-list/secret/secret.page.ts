23f3527e839449700f3c869bcadcf957ba98b435
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-03-16 08:53:59 +0100

    Update

--- src/app/secret-list/secret/secret.page.ts
+++ src/app/secret-list/secret/secret.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { SecretService } from '../../services/secret.service';
 
 @Component({
   selector: 'app-secret',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./secret.page.scss'],
 })
 export class SecretPage implements OnInit {
+  modif!: boolean;
+  secret: any = null;
 
-  constructor() { }
+  constructor(
+    private alertCtrl : AlertController,
+    private route: ActivatedRoute,
+    private Secret: SecretService,
+    private toastCtrl: ToastController,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.modif = false;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.Secret.get(id).subscribe((value: any) => {
+      this.secret = value;
+    });
   }
 
+ 
 }
